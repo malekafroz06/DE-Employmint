68429394305cc59d9189cb915751eec316e62396
Ashruti08 <95561210+Ashruti08@users.noreply.github.com>
2025-09-06 10:42:43 +0530

    aashruti added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,6 @@
-
 import React, { useState } from "react";
 import BannerBackground from "../../Assets/home-banner-background.png";
-import BannerVideo from "../../Assets/Video.mp4";
+import BannerImage from "../../Assets/e2.jpg";
 import Navbar from "../Navbar/Navbar";
 import { FiArrowRight } from "react-icons/fi";
 import './Home.css';
@@ -13,76 +12,75 @@ import { Work } from "@mui/icons-material";
 import Testimonial from "../Testimonial/Testimonial";
 
 const Home = () => {
-
-    const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState("");
+  
   return (
     <div className="home-container">
       {/* <Navbar /> */}
-      <div className="home-banner-container">
-        <div className="home-bannerImage-container">
-          <img src={BannerBackground} alt="" />
-        </div>
-        <div className="home-text-section">
-          <h1 className="primary-heading">
+      
+      {/* FIRST SECTION WITH BACKGROUND IMAGE */}
+      <div className="banner-section" style={{
+         backgroundImage: `url(${BannerImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: '4rem'
+      }}>
+        <div className="text-content">
+          <h1 className="primary-heading" >
             Got Talent ?<br />Meet Opportunity
           </h1>
-
+           
           <p className="primary-text">
             Company reviews. Salaries. Interviews. Jobs. Everything you need to know about your dream job.
           </p>
           <JobSearch/>
-           {/* Popular searches */}
-      <div className="popular-categories">
-        <span>Popular Searches: </span>
-        <ul className="list-category">
-          <li>
-            <a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                setKeyword("Wireframing");
-              }}
-            >
-              Wireframing
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                setKeyword("Responsive Design");
-              }}
-            >
-              Responsive Design
-            </a>
-          </li>
-        </ul>
-      </div>
-          {/* <button className="secondary-button" style={{marginTop: '2rem'}}>
-            Explore Jobs <FiArrowRight />
-          </button> */}
+           
+          {/* Popular searches */}
+          <div className="popular-categories">
+            <span color="white">Popular Searches: </span>
+            <ul className="list-category">
+              <li>
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setKeyword("Wireframing");
+                  }}
+                >
+                  Wireframing
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setKeyword("Responsive Design");
+                  }}
+                >
+                  Responsive Design
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="home-image-section">
-          <video 
-            src={BannerVideo} 
-            autoPlay 
-            loop 
-            muted 
-            playsInline
-          />
-        </div>
-         
       </div>
-     <RoleSection/>
-     <Workk />
-     <Testimonial/>
-     
+      
+      {/* OTHER SECTIONS - NO BACKGROUND */}
+      <div className="main-content">
+        <RoleSection/>
+        <Workk />
+        <Testimonial/>
+      </div>
+          
     </div>
-    
-   
+           
   );
 };
-
 
 export default Home;
